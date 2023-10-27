7aa888588ab92406e037f7aca548e44db955d1db
Christian Henkel <christian.henkel2@de.bosch.com>
2023-10-27 19:14:44 +0200

    linter fix
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- bt_live/src/bt_live/settings.py
+++ bt_live/src/bt_live/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,7 +76,7 @@ WSGI_APPLICATION = 'bt_live.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / "db.sqlite3"),
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
