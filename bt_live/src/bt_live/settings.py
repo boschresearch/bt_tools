c5fa4d735b8409cb166f266b5923ff3536549484
Christian Henkel <christian.henkel2@de.bosch.com>
2023-10-27 16:49:39 +0200

    handling error 'TypeError: argument of type 'PosixPath' is not iterable'
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- bt_live/src/bt_live/settings.py
+++ bt_live/src/bt_live/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'bt_live.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
