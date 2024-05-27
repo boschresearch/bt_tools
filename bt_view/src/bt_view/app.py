8711659dd2832163ab047505606732823a612494
Anton Utz <uta5fe@bosch.com>
2024-05-27 16:17:18 +0200

    improved webview with SSE for live updates
    Signed-off-by: Anton Utz <uta5fe@bosch.com>

--- bt_view/src/bt_view/app.py
+++ bt_view/src/bt_view/app.py
@@ -1,25 +1,38 @@
 from flask import Flask, render_template, Response
-import time
+import redis_server
+from flask_sse import sse
 import os
 
+
+
+# class App():
+#     def __init__(self) -> None:
+#         self.app = Flask(__name__)
+#         self.app.debug = True
+
 app = Flask(__name__)
+app.register_blueprint(sse, url_prefix='/stream')
+app.config["REDIS_URL"] = "redis://localhost:6379/"
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    abc = 'abc'
+    return render_template('index.html').replace('<div id="content"></div>', f'<div id="content">{abc}</div>')
 
 
-def generate():
-    while True:
-        # Simulate data update
-        with open(os.getcwd() + '/live_visualization.svg', 'r', encoding='utf-8') as f:
-            svg_content = f.read().replace('\n', '')
-        data = f"data: {svg_content}\n\n"
-        yield data
-        time.sleep(1)
+def send_sse_message(data):
+    with app.app_context():
+        sse.publish(data, type='update')
+
+# def generate():
+#     # Simulate data update
+#     with open(os.getcwd() + '/live_visualization.svg', 'r', encoding='utf-8') as f:
+#         svg_content = f.read().replace('\n', '')
+#     data = f"data: {svg_content}\n\n"
+#     yield data
 
 
-@app.route('/events')
-def events():
-    return Response(generate(), content_type='text/event-stream')
+# @app.route('/events')
+# def events():
+#     return Response(generate(), content_type='text/event-stream')
