682e2b61c16202a6b86c3e0afb523b4a11166807
Christian Henkel <christian.henkel2@de.bosch.com>
2024-08-12 15:40:26 +0200

    logo path
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ html_theme_options = {
     'style_nav_header_background': '#ED6A5A'
 }
 html_static_path = ['_static']
-html_logo = 'logo.png'
+html_logo = '_static/logo.png'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
